9fb33cbdc28a6da0c2269a6a71391718dff46db0
dlagur <87896627+dlagur@users.noreply.github.com>
2023-09-03 15:57:59 +0900

    Create 2294_.py

---
+++ dynamicprogram/2294_.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+coin = [int(input()) for _ in range(n)]
+
+
+dp = [10001]*(k+1)
+
+dp[0] = 0
+
+for i in coin:
+    for j in range(i, k+1):
+        if dp[j] > 0:
+            dp[j] = min(dp[j], dp[j-i] + 1)
+        
+
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
