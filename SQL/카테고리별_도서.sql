0d6db05b2536813d2bff312a583e129be32c4aa0
dlagur <87896627+dlagur@users.noreply.github.com>
2024-03-24 10:05:10 +0900

    Delete SQL directory

--- SQL/카테고리별_도서.sql
+++
@@ -1,7 +0,0 @@
-SELECT B.category, sum(S.sales) TOTAL_SALES
-FROM BOOK B
-INNER JOIN BOOK_SALES S
-ON B.book_id = S.book_id
-WHERE DATE_FORMAT(S.sales_date, '%Y-%m') = '2022-01'
-GROUP BY B.category
-ORDER BY B.category
