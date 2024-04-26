cae13d04b49cfabaa2c5b7b2da39a8c9272b1544
dlagur <qwer235611@gmail.com>
2024-04-26 11:19:58 +0900

    BFS 1문제

---
+++ DFS&BFS/1012_유기농배추.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+# bfs --> 최소한의 필요한 지렁이 수
+def bfs(x, y):
+    q = deque([])
+    q.append((x, y))
+    graph[x][y] = 0
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if (0 <= nx < n) and (0 <= ny < m) and graph[nx][ny] == 1:
+                q.append((nx, ny))
+                graph[nx][ny] = 0
+
+# main
+t = int(input())
+
+for _ in range(t):
+    m, n, k = map(int, input().split())
+
+    graph = [[0]*m for _ in range(n)]
+
+    for _ in range(k):
+        i, j = map(int, input().split())
+        graph[j][i] = 1
+
+    count = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                count += 1
+
+    print(count)
+
