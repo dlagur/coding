4a052a51362cb699ad959543e6dee70352a88b27
dlagur <qwer235611@gmail.com>
2024-09-21 18:05:16 +0900

    코테 1문제

---
+++ 귤고르기.py
@@ -0,0 +1,21 @@
+def solution(k, tangerine):
+    answer = 0
+    # 딕셔너리 생성
+    g = {}
+    # 귤 갯수 확인
+    for t in tangerine:
+        g[t] = g.get(t, 0) + 1
+
+    g = list(g.values())
+    g.sort(reverse=True)  # O(nlogn)
+    # 그리디 방식으로 k를 만족시키는 최소 귤 종류 선정
+    # 가장 많은 개수를 가진 귤의 갯수부터 정리
+
+    n = 0
+    for i in g:
+        answer += 1
+        n += i
+        if n >= k:
+            break
+
+    return answer
